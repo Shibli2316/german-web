2c46ee3ace919656cc39adc023e168bd1df4d83e
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2025-03-01 02:07:00 +0100

    typo fixed

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
         <div class="mr-auto place-self-center lg:col-span-7">
             <h1
                 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
-                Learning German <br>uick & easy.</h1>
+                Learning German <br>quick & easy.</h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">This is a 
                 free and open-source platform where you can learn german with your freinds and companions.</p>
             <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
